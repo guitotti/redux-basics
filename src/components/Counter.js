69deaf9e1390ca37dd00ef39845fef2a0b4708c6
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-10-21 08:43:54 -0300

    feat: adding useSelector react-redux hook to Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,19 @@
+import { useSelector } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  // inside useSelector: it will call a function that receives the state managed by Redux and return which part of the state we want
+  // the function is executed by Redux
+  // using useSelector, React-Redux will automatically set up a subscription to the Redux Store for this component 
+  const counter = useSelector(state => state.counter)
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter.value}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
